f2fa47c960dc8b88ddc9bdb793d71b04ed66167e
Anthony Dugarte <toonny1998@gmail.com>
2019-10-11 10:50:18 -0400

    Cards example

--- lib/main.dart
+++ lib/main.dart
@@ -28,33 +28,23 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future _showAlter(String str) async {
-    String s = await showDialog(
-        context: context,
-        builder: (BuildContext ctx) => AlertDialog(
-              title: Text(str),
-              actions: <Widget>[
-                FlatButton(
-                  child: Text("Ok"),
-                  onPressed: () => Navigator.pop(ctx, "This is my return"),
-                ),
-              ],
-            ));
-
-    debugPrint(s);
-
-    return s;
-  }
-
   @override
   Widget build(BuildContext context) => Center(
         child: Column(
           children: <Widget>[
-            Text("Hello"),
-            RaisedButton(
-              onPressed: () => _showAlter("uwu"),
-              child: Text("Click"),
-            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(32),
+                child: Column(
+                  children: <Widget>[
+                    Image.network(
+                      "http://pluspng.com/img-png/random-png-image-mabel-s-sweater-creator-random-gnome-png-gravity-falls-wiki-fandom-powered-by-wikia-510.png",
+                    ),
+                    Text("Gnomo"),
+                  ],
+                ),
+              ),
+            )
           ],
         ),
       );
