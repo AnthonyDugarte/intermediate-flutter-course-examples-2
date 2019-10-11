048167fa2fa14bfb0654d080874256e7c37ba26c
Anthony Dugarte <toonny1998@gmail.com>
2019-10-11 10:19:20 -0400

    Extract value from dialog example

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,67 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: MyApp(),
+        home: BaseApp(),
         title: "MIddle class Testing app",
       ),
     );
+
+class BaseApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Example App"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: MyApp(),
+      ),
+    );
+  }
+}
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> {
-  final GlobalKey<ScaffoldState> _scaffold = GlobalKey<ScaffoldState>();
+enum Options { YES, NO }
 
-  void _showSnak() {
-    _scaffold.currentState.showSnackBar(
-      SnackBar(
-        content: Text("OH MY GAAAAH"),
+class _MyAppState extends State<MyApp> {
+  String _value;
+
+  Future _event() async {
+    Options op = await showDialog(
+      context: context,
+      builder: (_) => SimpleDialog(
+        title: Text("Dialog"),
+        children: <Widget>[
+          SimpleDialogOption(
+            child: Text("Ok"),
+            onPressed: () => Navigator.pop(context, Options.YES),
+          ),
+          SimpleDialogOption(
+            child: Text("Cancel"),
+            onPressed: () => Navigator.pop(context, Options.NO),
+          )
+        ],
       ),
     );
+
+    setState(() => _value = op.toString());
   }
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        key: _scaffold,
-        appBar: AppBar(
-          title: Text("My Example App"),
-        ),
-        body: Container(
-          padding: EdgeInsets.all(32),
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                RaisedButton(
-                  onPressed: _showSnak,
-                  child: Text("Click"),
-                ),
-              ],
+  Widget build(BuildContext context) => Center(
+        child: Column(
+          children: <Widget>[
+            Text(_value),
+            RaisedButton(
+              onPressed: _event,
+              child: Text("Click"),
             ),
-          ),
+          ],
         ),
       );
 }
